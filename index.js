1dfe412c9c9db1b52d5f39ba8d25aa40b85920f9
ibrahim aljarad <ibrahimealkalil@gmail.com>
2020-08-25 00:00:34 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Your code here
+class Cat {
+    constructor(name,sex){
+        this.name=name;
+        this.sex=sex;
+    }
+   naming(){
+       return this.name
+   }
+    speak(){
+        return `${this.name} says meow!`
+    }
+  }
+cat.naming();
+cat.speak();
+
+
+class Dog {
+    constructor(name,sex){
+        this.name=name;
+        this.sex=sex;
+    }
+//     naming(){
+//         return this.name
+//     }
+//      speak(){
+//          return `${this.name} says woof!`
+//      }
+//    }
+//  Dog.naming();
+//  Dog.speak();
+
+//   class Bird {
+//     constructor(name,sex){
+//         this.name=name;
+//         this.sex=sex;
+//     }
+//     naming(){
+//         return this.name
+//     }
+//      speak(){
+//          return `${this.name} says meow!`
+//      }
+//    }
+//  cat.naming();
+//  cat.speak();
